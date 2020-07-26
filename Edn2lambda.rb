cb3537dcd417e0fe56b03a9b52688a5588d43ed8
Chetan Kaushik <dynamitechetan@gmail.com>
2020-07-27 00:28:56 +0530

    Update Edn2lambda.rb

--- Edn2lambda.rb
+++ Edn2lambda.rb
@@ -3,6 +3,8 @@ class Edn2lambda < Formula
   homepage "https://github.com/dynamitechetan/edn-to-lambda"
   url "https://violet-parent.surge.sh/edn2lambda.tar.gz"
   sha256 "1bf89767cd354f60af4a95b7e483f4f4f03d23bf827b7361d872bab7c5ac976d"
+  
+  revision 3
 
   bottle :unneeded
 
