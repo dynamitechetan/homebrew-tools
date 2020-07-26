e2909dd78701b3e8f75eb699c58284a3cb438b29
Chetan Kaushik <dynamitechetan@gmail.com>
2020-07-27 00:27:50 +0530

    Update Edn2lambda.rb

--- Edn2lambda.rb
+++ Edn2lambda.rb
@@ -1,13 +1,16 @@
 class Edn2lambda < Formula
   desc "testing taps"
   homepage "https://github.com/dynamitechetan/edn-to-lambda"
-  url "http://high-birthday.surge.sh/edn2lambda-1.0.0.tar.gz"
-  sha256 "e366293bd911859ff8aa47fac6b7d6285cb39359001665dc0ef54762abb0cdf0"
+  url "https://violet-parent.surge.sh/edn2lambda.tar.gz"
+  sha256 "1bf89767cd354f60af4a95b7e483f4f4f03d23bf827b7361d872bab7c5ac976d"
 
   bottle :unneeded
 
+  depends_on "openjdk"
+
   def install
-    bin.install "edn2lambda"
+    libexec.install Dir["*"]
+    (bin/"edn2lambda").write_env_script libexec/"bin/edn2lambda", :JAVA_HOME => Formula["openjdk"].opt_prefix
   end
 
   test do
