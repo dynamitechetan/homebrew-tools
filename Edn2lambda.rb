bf2224a5fb42f2909c5a3c4752f1bc59e43e0784
Chetan Kaushik <dynamitechetan@gmail.com>
2020-07-27 10:36:05 +0530

    Update Edn2lambda.rb

--- Edn2lambda.rb
+++ Edn2lambda.rb
@@ -1,7 +1,7 @@
 class Edn2lambda < Formula
   desc "testing taps"
   homepage "https://github.com/dynamitechetan/edn-to-lambda"
-  url "https://violet-parent.surge.sh/edn2lambda-1.0.0.tar.gz"
+  url "https://github.com/dynamitechetan/homebrew-tools/releases/download/1.0.0/edn2lambda-1.0.0.tar.gz"
   sha256 "461963e681866fd6f3b0dafac5b3fac696e1deddb6c8df9014678cff813b2101"
   
   revision 4
